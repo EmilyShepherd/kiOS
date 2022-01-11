1b5c9718f078d57c7be6c380fbe119c24c29ef9e
Emily Shepherd <emily@redcoat.dev>
2022-01-11 00:54:54 +0000

    Add pkgs for runc and cri-o

--- scripts/build-pkg.sh
+++ scripts/build-pkg.sh
@@ -24,6 +24,12 @@ case $type in
 esac
 
 case $url in
+  github)
+    url=https://github.com/${project}/${pkg}/archive/refs/tags/v${version}.tar.gz
+    ;;
+esac
+
+case $url in
   *.bz2)
     extract=j ;;
   *.gz)
@@ -41,13 +47,20 @@ curl -L $url | tar -x$extract -C src --strip-components 1
 
 cd src
 
-export PKG_CONFIG_PATH=${BUILD_DIR}/lib/pkgconfig 
-export CC=$(which ${TARGET}-gcc)
+export GOARCH=arm64
+export CROSS_COMPILE=aarch64
+export PREFIX=$BUILD_DIR
 
 case $type in
   configure)
+    export PKG_CONFIG_PATH=${BUILD_DIR}/lib/pkgconfig 
+    export CC=$(which ${TARGET}-gcc)
     ./configure --prefix=${BUILD_DIR} --host=${TARGET} $configure_flags
     make
     make install
+    ;;
+  make)
+    make
+    make ${install_target:-install}
     ;;
 esac
