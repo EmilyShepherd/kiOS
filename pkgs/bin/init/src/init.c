daf975383bfdb2594c5c3e7ebde223a5da00c049
Emily Shepherd <emily@redcoat.dev>
2023-02-15 23:22:01 +0100

    Init code tidy up
    Add comments and re-order init.c to make more sense.

--- pkgs/bin/init/src/init.c
+++ pkgs/bin/init/src/init.c
@@ -33,15 +33,24 @@
 #define KUBELET_CONFIG "/var/lib/kubelet/config.yaml"
 #define INIT_MANIFEST "/var/etc/kubernetes/manifests/init.yaml"
 
-static void mount_fs();
-static void bring_if_up(const char *);
-void wait_for_path(const char* file);
-
+/**
+ * File Exists
+ *
+ * Helper function to check if the given file exists
+ */
 static int fexists(const char *path) {
   struct stat _;
   return !stat(path, &_);
 }
 
+/**
+ * Start Exe
+ *
+ * Helper function to fork, update stdout and stderr to the given log
+ * file, and then exec the given binary. This is used to startup kubelet
+ * and crio, both of which need to log to kubelet.log and crio.log,
+ * respectively.
+ */
 static pid_t start_exe(const char *exe, const char *log, char * const *argv) {
   pid_t pid;
   pid = fork();
@@ -59,6 +68,12 @@ static pid_t start_exe(const char *exe, const char *log, char * const *argv) {
   execv(exe, argv);
 }
 
+/**
+ * Set Hostname From File
+ *
+ * Checks to see if /etc/hostname exists and contains content - if it
+ * does the hostname is updated.
+ */
 static void set_hostname_from_file(void) {
   char hostname[HOST_NAME_MAX];
   FILE *fp = fopen("/etc/hostname", "r");
@@ -70,113 +85,16 @@ static void set_hostname_from_file(void) {
   }
 }
 
-static void start_container_runtime(void) {
-  // wait_for_path only works when the directory the expected file will
-  // be in already exists (it does not recursively check up). Ensure
-  // that the directory exists here.
-  mkdir("/var/run/crio", 0600);
-  char const *nullArgs[] = {"/bin/crio", NULL};
-  pid_t crio = start_exe("/bin/crio", CRIO_LOG, nullArgs);
-  wait_for_path(CRIO_SOCK);
-
-  if (!fexists(KUBELET_CONFIG)) {
-    char const *initArgs[] = {
-      "/bin/kubelet",
-      "--container-runtime-endpoint", "unix:///var/run/crio/crio.sock",
-      "--pod-manifest-path=/etc/kubernetes/manifests",
-      NULL
-    };
-    pid_t initkubelet = start_exe("/bin/kubelet", KUBELET_LOG, initArgs);
-
-    wait_for_path(KUBELET_CONFIG);
-    kill(initkubelet, SIGTERM);
-    waitpid(initkubelet, NULL, 0);
-  }
-
-  char * const kubeletArgs[] = {
-    "/bin/kubelet",
-    "--config", KUBELET_CONFIG,
-    "--kubeconfig", "/etc/kubernetes/kubelet.conf",
-    "--bootstrap-kubeconfig", "/etc/kubernetes/bootstrap-kubelet.conf",
-    "--container-runtime-endpoint", "unix:///var/run/crio/crio.sock",
-    NULL
-  };
-  pid_t kubelet = start_exe("/bin/kubelet", KUBELET_LOG, kubeletArgs);
-
-  while (1) {
-    wait();
-  }
-}
-
 /**
- * Enable IP Forwarding
+ * Wait for Path
  *
- * Although kiOS tries to be relatively unopinionated, enabling ip
- * forwarding is almost always a requirement for Pod / Service
- * Networking to work as expected, so we will set that sysctl here so as
- * to not require another service to set it. This is only set at boot,
- * so its value can be overriden at runtime by a sufficiently privileged
- * service, if required.
+ * Waits for a path to exist by setting up an inotify watcher for its
+ * parent directory.
+ *
+ * WARNING: This will never return, unless the file comes into
+ * existence. Only use this for files which are expected to exist, but
+ * may not yet early in the boot process.
  */
-void enable_ip_forwarding(void) {
-  FILE *fp = fopen("/proc/sys/net/ipv4/ip_forward", "w");
-  fputs("1", fp);
-  fclose(fp);
-}
-
-int main(int argc, char **argv) {
-  char *console;
-  console = getenv("CONSOLE");
-  if (!console) {
-    console = getenv("console");
-  }
-
-  if (console) {
-    int fd = open(console, O_RDWR | O_NONBLOCK | O_NOCTTY);
-    if (fd >= 0) {
-      dup2(fd, STDIN_FILENO);
-      dup2(fd, STDOUT_FILENO);
-      dup2(fd, STDERR_FILENO);
-      close(fd);
-    }
-  }
-
-  printf("Kios Init\n");
-
-  bring_if_up("lo");
-  bring_if_up("eth0");
-
-  mount_fs();
-  enable_ip_forwarding();
-
-  char *datapart = getenv("datapart");
-  if (!datapart) {
-    datapart = DEFAULT_DATAPART;
-  }
-
-  wait_for_path(datapart);
-  mount(datapart, "/tmp", "ext4", 0, 0);
-  bind_mount("/tmp/modules", "/lib/modules", STATIC_FLAGS);
-  bind_mount("/tmp/log", "/var/log", MS_NODEV | MS_NOEXEC | MS_NOSUID);
-  bind_mount("/tmp/lib", "/var/lib", 0);
-  bind_mount("/tmp/etc", "/etc", MS_NODEV | MS_NOEXEC | MS_NOSUID);
-  umount("/tmp");
-
-  set_hostname_from_file();
-
-  putenv("PATH=/bin");
-  start_container_runtime();
-}
-
-static void bring_if_up(const char *iff) {
-  int ip = socket(PF_INET, SOCK_DGRAM, 0);
-  struct ifreq ifr;
-  strncpy(&ifr.ifr_name, iff, 16);
-  ioctl(ip, SIOCGIFFLAGS, &ifr);
-  ifr.ifr_flags |= IFF_UP;
-  ioctl(ip, SIOCSIFFLAGS, &ifr);
-}
-
 void wait_for_path(const char *path) {
   // If the file exists, we are good to go
   if (fexists(path)) {
@@ -231,6 +149,117 @@ end:
   close(fd);
 }
 
+/**
+ * Start Container Runtime
+ *
+ * Starts up crio, waits for it to be ready, then starts up kubelet. If
+ * the kubelet config file does not exist, kubelet will be started in
+ * standalone mode. Whilst running in standalone mode, if the config
+ * file is ever created (eg by an init container setting one up),
+ * kubelet will be stopped, and restarted in API server mode.
+ */
+static void start_container_runtime(void) {
+  // wait_for_path only works when the directory the expected file will
+  // be in already exists (it does not recursively check up). Ensure
+  // that the directory exists here.
+  mkdir("/var/run/crio", 0600);
+  char const *nullArgs[] = {"/bin/crio", NULL};
+  pid_t crio = start_exe("/bin/crio", CRIO_LOG, nullArgs);
+  wait_for_path(CRIO_SOCK);
+
+  if (!fexists(KUBELET_CONFIG)) {
+    char const *initArgs[] = {
+      "/bin/kubelet",
+      "--container-runtime-endpoint", "unix:///var/run/crio/crio.sock",
+      "--pod-manifest-path=/etc/kubernetes/manifests",
+      NULL
+    };
+    pid_t initkubelet = start_exe("/bin/kubelet", KUBELET_LOG, initArgs);
+
+    wait_for_path(KUBELET_CONFIG);
+    kill(initkubelet, SIGTERM);
+    waitpid(initkubelet, NULL, 0);
+  }
+
+  char * const kubeletArgs[] = {
+    "/bin/kubelet",
+    "--config", KUBELET_CONFIG,
+    "--kubeconfig", "/etc/kubernetes/kubelet.conf",
+    "--bootstrap-kubeconfig", "/etc/kubernetes/bootstrap-kubelet.conf",
+    "--container-runtime-endpoint", "unix:///var/run/crio/crio.sock",
+    NULL
+  };
+  pid_t kubelet = start_exe("/bin/kubelet", KUBELET_LOG, kubeletArgs);
+
+  while (1) {
+    wait();
+  }
+}
+
+/**
+ * Enable IP Forwarding
+ *
+ * Although kiOS tries to be relatively unopinionated, enabling ip
+ * forwarding is almost always a requirement for Pod / Service
+ * Networking to work as expected, so we will set that sysctl here so as
+ * to not require another service to set it. This is only set at boot,
+ * so its value can be overriden at runtime by a sufficiently privileged
+ * service, if required.
+ */
+void enable_ip_forwarding(void) {
+  FILE *fp = fopen("/proc/sys/net/ipv4/ip_forward", "w");
+  fputs("1", fp);
+  fclose(fp);
+}
+
+/**
+ * Start Console
+ *
+ * Opens up the console character device, if specified by the CONSOLE/
+ * console environment variable, and sets it up as the init process'
+ * stdin, stdout, and stderr.
+ */
+void start_console(void) {
+  char *console;
+  console = getenv("CONSOLE");
+  if (!console) {
+    console = getenv("console");
+  }
+
+  if (console) {
+    int fd = open(console, O_RDWR | O_NONBLOCK | O_NOCTTY);
+    if (fd >= 0) {
+      dup2(fd, STDIN_FILENO);
+      dup2(fd, STDOUT_FILENO);
+      dup2(fd, STDERR_FILENO);
+      close(fd);
+    }
+  }
+}
+
+/**
+ * Bring Interface Up
+ *
+ * Updates the given interface name to be UP.
+ */
+static void bring_if_up(const char *iff) {
+  int ip = socket(PF_INET, SOCK_DGRAM, 0);
+  struct ifreq ifr;
+  strncpy(&ifr.ifr_name, iff, 16);
+  ioctl(ip, SIOCGIFFLAGS, &ifr);
+  ifr.ifr_flags |= IFF_UP;
+  ioctl(ip, SIOCSIFFLAGS, &ifr);
+}
+
+/**
+ * Mount Filesystem
+ *
+ * Sets up and mounts kiOS' base OS file system. This includes most
+ * "standard" things like devtmpfs, sysfs, proc, etc as well as a few
+ * sensible tmpfs directories.
+ *
+ * Cgroup v1 mounts are also created.
+ */
 static void mount_fs() {
   // Base system mounts
   init_mount("proc", "/proc", MS_NODEV);
@@ -288,4 +317,56 @@ static void mount_fs() {
     }
   }
   fclose(fp);
+}
+
+/**
+ * Mount Datapart
+ *
+ * Mounts the data partition folders. The data partition to use is
+ * normally given by the datapart kernel command line parameter, however
+ * kiOS will use an internal default if nothing else is provided.
+ *
+ * In the data partition, kiOS expects the following directories to
+ * exist:
+ *   modules - mounted to /lib/modules
+ *   log - mounted to /var/log
+ *   lib - mounted to /var/lib
+ *   etc - mounted to /etc
+ *
+ * Other files and directories in the datapartition are ignored and are
+ * not accessible at runtime by init-created mounts.
+ */
+void mount_datapart(void) {
+  char *datapart = getenv("datapart");
+  if (!datapart) {
+    datapart = DEFAULT_DATAPART;
+  }
+
+  wait_for_path(datapart);
+  mount(datapart, "/tmp", "ext4", 0, 0);
+  bind_mount("/tmp/modules", "/lib/modules", STATIC_FLAGS);
+  bind_mount("/tmp/log", "/var/log", MS_NODEV | MS_NOEXEC | MS_NOSUID);
+  bind_mount("/tmp/lib", "/var/lib", 0);
+  bind_mount("/tmp/etc", "/etc", MS_NODEV | MS_NOEXEC | MS_NOSUID);
+  umount("/tmp");
+}
+
+int main(int argc, char **argv) {
+  start_console();
+  printf("Kios Init\n");
+
+  // We want to enable networking as quickly as possible so that the
+  // kernel can be getting on with SLAAC, if available on the network,
+  // whilst we are busy with other things.
+  bring_if_up("lo");
+  bring_if_up("eth0");
+
+  mount_fs();
+  enable_ip_forwarding();
+  mount_datapart();
+
+  set_hostname_from_file();
+
+  putenv("PATH=/bin");
+  start_container_runtime();
 }
