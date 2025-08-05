1b4f25f4ea891ec0914fdd5de6aa7fc2ffb240d8
Emily Shepherd <emily@redcoat.dev>
2025-08-06 00:18:19 +0100

    Remove wait-for-time image
    This was a previous weird hack that was in place to fix the broken time
    when booting from a raspberry pi. We have not used this in a long time
    and this area of the codebase has not been maintained. If this is ever
    needed again, we will spin it up as a seperate project in line with the
    new structure.

--- pkgs/images/wait-for-time/src/main.c
+++
@@ -1,8 +0,0 @@
-
-#include "wait_for_time.h"
-
-int main(int argc, char **argv) {
-  wait_for_sensible_time();
-
-  return 0;
-}
--- pkgs/images/wait-for-time/src/wait_for_time.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef _WAIT_FOR_TIME_H
-#define _WAIT_FOR_TIME_H 1
-
-void wait_for_sensible_time();
-
-#endif /* wait_for_time.h */
